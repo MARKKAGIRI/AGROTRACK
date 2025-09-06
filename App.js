b178a3a6b66be4340591fe0f4a131b3d864837a4
WanyekiVick <wanyekivick419@gmail.com>
2025-09-06 15:50:34 +0300

    Linked navigation

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import AnalyticsScreen from './src/screens/Analytics/Analytics';
+import AppNavigator from './src/navigation/AppNavigator';
 // import { StatusBar } from 'expo-status-bar';
 // import { StyleSheet, Text, View } from 'react-native';
 // import AppNavigator from './src/navigation/AppNavigator';
 // import "./global.css"
 
 export default function App() {
-  return <AnalyticsScreen />;
+  return <AppNavigator />;
 }
 
