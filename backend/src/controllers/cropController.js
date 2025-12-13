15cd0f7ad386a378327e2ac01349fd22ac7004ae
rodgers-munene <munenerodgers72@gmail.com>
2025-12-13 14:54:42 +0300

    endpoint to allow getting a single crop from the crops table by id

--- backend/src/controllers/cropController.js
+++ backend/src/controllers/cropController.js
@@ -22,6 +22,27 @@ const getAllCrops = async (req, res) => {
   }
 };
 
+// get a single crop by id 
+
+const getCropById = async(req, res) => {
+  try {
+    const { cropId } = req.params
+
+    const crop = await prisma.crops.findUnique({ where: { id: Number(cropId) }})
+
+    return res.json({
+      success: true,
+      crop
+    })
+  } catch (error) {
+    console.error("Failed to fetch crops:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Error fetching crops",
+    });
+  }
+} 
+
 // Create a crop (used for seeding or admin)
 const createCrop = async (req, res) => {
   try {
@@ -48,5 +69,6 @@ const createCrop = async (req, res) => {
 
 module.exports = {
   getAllCrops,
+  getCropById,
   createCrop,
 };
--- backend/src/routes/cropRoutes.js
+++ backend/src/routes/cropRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   getAllCrops,
   createCrop,
+  getCropById,
 } = require("../controllers/cropController");
 const { tokenValidator } = require("../middleware/authMiddleware");
 
 // crops
 router.get("/", tokenValidator, getAllCrops);   // GET /crops
+router.get("/:cropId", tokenValidator, getCropById)
 router.post("/", tokenValidator, createCrop);  // POST /crops
 
 module.exports = router;
